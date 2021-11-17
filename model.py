39db9e6c0611fb16db186237ca79c1609b796bcf
Xinran Wu <535232735@qq.com>
2021-11-17 21:30:11 +0800

    Update model.py

--- model.py
+++ model.py
@@ -13,9 +13,9 @@ class MemN2N(nn.Module):
         self.embed_size = params.embed_size
         self.memory_size = params.memory_size
         self.num_hops = params.num_hops
-        self.use_bow = params.use_bow
-        self.use_lw = params.use_lw
-        self.use_ls = params.use_ls
+        self.use_bow = params.use_bow #使用的bag-of-words或pe
+        self.use_lw = params.use_lw #使用adjacent or not
+        self.use_ls = params.use_ls #使用linear start or not
         self.vocab = vocab
 
         # create parameters according to different type of weight tying
@@ -47,24 +47,32 @@ class MemN2N(nn.Module):
         self.TC = nn.Parameter(I.normal_(torch.empty(self.memory_size, self.embed_size), 0, 0.1))
 
     def forward(self, story, query):
-        sen_size = query.shape[-1]
+        sen_size = query.shape[-1] #句子长度
         weights = self.compute_weights(sen_size)
-        state = (self.B(query) * weights).sum(1)
+        state = (self.B(query) * weights).sum(1) # 位置权重乘以embedding得到query的表示
 
         sen_size = story.shape[-1]
         weights = self.compute_weights(sen_size)
         for i in range(self.num_hops):
+            # (self.A[i](story.view(-1, sen_size)) * weights).sum(1) 求每个sentence的表示，与state的求法一致
+            # view之后应该是sen_num乘emb_size
             memory = (self.A[i](story.view(-1, sen_size)) * weights).sum(1).view(
                 *story.shape[:-1], -1)
-            memory += self.TA
+            memory += self.TA # dummy memories/random noise
+            
             output = (self.C[i](story.view(-1, sen_size)) * weights).sum(1).view(
                 *story.shape[:-1], -1)
             output += self.TC
-
-            probs = (memory @ state.unsqueeze(-1)).squeeze()
+            # 求memory和state的相似度
+            probs = (memory @ state.unsqueeze(-1)).squeeze() # @等价于Torch.matmul()
+            
+            #如果不是linear start 在每层都要进行softmax
             if not self.use_ls:
                 probs = F.softmax(probs, dim=-1)
+                
+            # 将weight与output相乘
             response = (probs.unsqueeze(1) @ output).squeeze()
+            
             if self.use_lw:
                 state = self.H(response) + state
             else:
@@ -74,9 +82,9 @@ class MemN2N(nn.Module):
 
     def compute_weights(self, J):
         d = self.embed_size
-        if self.use_bow:
+        if self.use_bow: # 只使用词袋模型 就全部赋值为1——位置的权重是1
             weights = torch.ones(J, d)
         else:
-            func = lambda j, k: 1 - (j + 1) / J - (k + 1) / d * (1 - 2 * (j + 1) / J)    # 0-based indexing
+            func = lambda j, k: 1 - (j + 1) / J - (k + 1) / d * (1 - 2 * (j + 1) / J)    # 0-based indexing 考虑位置编码
             weights = torch.from_numpy(np.fromfunction(func, (J, d), dtype=np.float32))
         return weights.cuda() if torch.cuda.is_available() else weights
